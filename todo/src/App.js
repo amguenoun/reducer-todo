d9fc6cd8b0156c91ec1f0c26a1595f6b8453f566
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-10 15:56:18 -0400

    added form and can now sucessfully add to state

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
+
+//import reducer
+import { initialState, todoReducer } from './reducers/todoReducer';
+
+//import stylesheets
 import './App.css';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  console.log(state);
+
+  const handleInput = (e) => {
+    setNewTodo(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_TODO', payload: { id: Date.now(), name: newTodo, completed: false } });
+    setNewTodo('');
+  }
+
   return (
     <div className="App">
-      Test
+      <h1>React Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <input name='name' placeholder='Add Todo Item' value={newTodo} onChange={handleInput} />
+        <button type='submit'>Add Todo</button>
+      </form>
     </div>
   );
 }
